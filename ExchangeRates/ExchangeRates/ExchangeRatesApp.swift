078c7df37ce6eeff72df27af350ad1b71d1ac19c
Gemuele (Gem) Aludino <g.aludino@gmail.com>
2024-11-23 22:58:19 -0500

    Xcode project creation and initial configuration

---
+++ ExchangeRates/ExchangeRates/ExchangeRatesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExchangeRatesApp.swift
+//  ExchangeRates
+//
+//  Created by Gemuele Aludino on 11/23/24.
+//
+
+import SwiftUI
+
+@main
+struct ExchangeRatesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ExchangeRates/ExchangeRatesTests/ExchangeRatesTests.swift
@@ -0,0 +1,16 @@
+//
+//  ExchangeRatesTests.swift
+//  ExchangeRatesTests
+//
+//  Created by Gemuele Aludino on 11/23/24.
+//
+
+import Testing
+
+struct ExchangeRatesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
